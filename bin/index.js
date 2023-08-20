d57fde1bb512fbb9f75dc6fab153ed06a23863be
weiqifu <weiqifu@tencent.com>
2023-08-20 22:41:58 +0800

    feat: 完成基础功能

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,49 @@
-const Validate = require("../dist/validate.cjs");
+import Validate from "../dist/validate.js";
 
 const obj = {
   name: "John",
-  phone: "1234567890",
+  mobile: "17602358181",
   pass: "123456",
-  confirm_pass: "123456",
+  father: {
+    name: "John father",
+  },
+  diy: "diy",
 };
 
 const rules = {
   name: [{ rule: "required", msg: "名称不能为空" }],
-  phone: [
+  mobile: [
     { rule: "required", msg: "手机号码不能为空" },
-    { rule: "phone", msg: "手机号码格式不对" },
+    { rule: "mobile", msg: "手机号码格式不对" },
   ],
   pass: [{ rule: "required", msg: "密码不能为空" }],
-  confirm_pass: [{ rule: "same:pass", msg: "两次密码不一致" }],
+  father: {
+    name: [{ rule: "required", msg: "名称不能为空" }],
+  },
+  diy: [
+    {
+      rule: (value, resolve, reject) => {
+        setTimeout(() => {
+          if (value === "diy") {
+            console.log("diy");
+            resolve();
+          } else {
+            reject({
+              rule: "check",
+              msg: "diy错误",
+            });
+          }
+        }, 1000);
+      },
+      msg: "diy",
+    },
+  ],
 };
 
-new Validate(obj, rules);
+new Validate(obj, rules)
+  .then(() => {
+    console.log("success");
+  })
+  .catch((err) => {
+    console.log("err", JSON.stringify(err));
+  });
