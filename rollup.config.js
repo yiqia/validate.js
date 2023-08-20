687343358f57877c5f4030346bd5d4969b086786
weiqifu <weiqifu@tencent.com>
2023-08-20 23:16:56 +0800

    fix: 修改文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/index.js",
@@ -18,5 +19,9 @@ export default {
       format: "iife",
     },
   ],
-  plugins: [resolve(), babel({ babelHelpers: "bundled" })],
+  plugins: [
+    resolve(),
+    babel({ exclude: "node_modules/**", babelHelpers: "bundled" }),
+    terser(),
+  ],
 };
